0b18055ef485b960ceb1a0a538912d5c961abf08
Kauan Santos <kauansantos83@gmail.com>
2021-05-03 22:31:00 -0300

    adc função excluir

--- controller/listaClientes-controller.js
+++ controller/listaClientes-controller.js
@@ -1,5 +1,5 @@
 import { clienteService } from '../service/cliente-service.js'
-const criaNovaLinha = (nome, email) =>  { 
+const criaNovaLinha = (nome, email, id) =>  { 
   const linhaNovoCliente = document.createElement('tr')
   const conteudo = `
       <td class="td" data-td>${nome}</td>
@@ -12,11 +12,23 @@ const criaNovaLinha = (nome, email) =>  {
                   </td> 
                   `
   linhaNovoCliente.innerHTML = conteudo
+  linhaNovoCliente.dataset.id = id
   return linhaNovoCliente
 }
 
-
 const tabela = document.querySelector('[data-tabela]')
+
+tabela.addEventListener('click', (evento) => {
+let ehBotaoDeletar = evento.target.className = 'botao-simples botao-simples--excluir'
+if(ehBotaoDeletar) {
+    const linhaCliente = evento.target.closest('[data-id]')
+    let id = linhaCliente.dataset.id
+    clienteService.removeCliente(id)
+    .then(() => {
+        linhaCliente.remove()
+        })
+    }
+})
 
 clienteService.listaClientes()
 .then(data => {
