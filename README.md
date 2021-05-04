c4090141d376f6bcf2a300ab31ef8dd1ab1468b8
Kauan Santos <kauansantos83@gmail.com>
2021-05-03 21:20:19 -0300

    adc cliente concluido

--- README.md
+++ README.md
@@ -1,7 +1,1 @@
-abrir a pasta admin no vscode e 
-rodar o json server: 
-```js
-npx json-server --watch db.json
-```
 
-Rodar: npx browser-sync start --server --file . --host --port 5000 --startPath telas/lista_cliente.html
--- service/cliente-service.js
+++ service/cliente-service.js
@@ -9,7 +9,7 @@ const criaCliente = (nome, email) => {
     return fetch(`http://localhost:3000/profile`, {
         method: 'POST',
         headers: {
-            'Content-Type' : 'aplication/json'
+            'Content-Type' : 'application/json'
         },
         body: JSON.stringify({
             nome: nome,
